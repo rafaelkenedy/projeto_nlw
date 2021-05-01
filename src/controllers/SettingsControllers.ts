2b63537e73dd2f1e15131c2bf24dc628872535eb
rafaelkenedy <k.rafaelalves@gmail.com>
2021-04-30 22:55:12 -0300

    Node-modules
    Node-modules

--- src/controllers/SettingsControllers.ts
+++ src/controllers/SettingsControllers.ts
@@ -1,27 +1,17 @@
 import {Request, Response } from "express"
-import { getCustomRepository } from "typeorm"
-import { SettingsRepository } from "../repositories/SettingsRepository"
+import { SettingsService } from "../services/SettingsService"
 
 class SettingsController{
 
   async create(request: Request, response: Response){
     const { chat, username} = request.body
 
-    const settingsRepository   = getCustomRepository(SettingsRepository)
-  
-    const settings = settingsRepository.create({
-      chat, 
-      username
-    })
-  
-    await settingsRepository.save(settings)
+    const settingsService = new SettingsService()
+
+    const settings = await settingsService.create({chat, username})   
   
     return response.json(settings)
-
-
   }
-
-
 }
 
 export { SettingsController }
